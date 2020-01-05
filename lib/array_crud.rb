7d9f0df1727891b56adaeefca8ce845ce934126b
Duncan Uruchima <duruchima1@gmail.com>
2020-01-05 21:08:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-[language, board, input, index]
+["language", "board", "input", "index"]
 end
 
 def add_element_to_end_of_array(array, element)
